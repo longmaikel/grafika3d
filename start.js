0d8a9a1288305ce91429047e9481e4eab471468c
maikel <kutaj74@gmail.com>
2023-04-16 11:58:43 +0200

    3.1 widoki podzielony, stereo, zwykly

--- start.js
+++ start.js
@@ -3,9 +3,12 @@ function start() {
 //Inicialize the GL contex
 	const gl = canvas.getContext("webgl2");
 	if (gl === null) {
-	alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-	return;
-}
+		alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+		return;
+	}
+
+	// ustawienie trybu wyswietlania obrazu
+	let tryb = 1;
 
 	//texture1 *****************************************************************************
 	const texture1 = gl.createTexture();
@@ -255,18 +258,69 @@ function draw() {
 		licznik = 0;
 	}
 
+	gl.clearColor(0, 0, 0, 1);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	switch (tryb) {
+		case 1: //normal
+			gl.viewport(0, 0, canvas.width, canvas.height);
+			// gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+			gl.activeTexture(gl.TEXTURE0);
+			gl.bindTexture(gl.TEXTURE_2D, texture1);
+			gl.drawArrays(gl.TRIANGLES, 0, 12);
+			// gl.activeTexture(gl.TEXTURE1);
+			gl.bindTexture(gl.TEXTURE_2D, texture2);
+			gl.drawArrays(gl.TRIANGLES, 12, 24);
+			break;
+		case 2: //stereo
+			gl.viewport(0, 0, canvas.width, canvas.height);
+			StereoProjection(-6, 6, -4.8, 4.8, 12.99, -100, 0, 13, -0.05);
+			gl.colorMask(true,false,false,false);
+
+			gl.activeTexture(gl.TEXTURE0);
+			gl.bindTexture(gl.TEXTURE_2D, texture1);
+			gl.drawArrays(gl.TRIANGLES, 0, 12);
+			// gl.activeTexture(gl.TEXTURE1);
+			gl.bindTexture(gl.TEXTURE_2D, texture2);
+			gl.drawArrays(gl.TRIANGLES, 12, 24);
+
+			gl.clear(gl.DEPTH_BUFFER_BIT);
+			StereoProjection(-6, 6, -4.8, 4.8, 12.99, -100, 0, 13, 0.05);
+			gl.colorMask(false,false,true,false);
+
+			gl.activeTexture(gl.TEXTURE0);
+			gl.bindTexture(gl.TEXTURE_2D, texture1);
+			gl.drawArrays(gl.TRIANGLES, 0, 12);
+			// gl.activeTexture(gl.TEXTURE1);
+			gl.bindTexture(gl.TEXTURE_2D, texture2);
+			gl.drawArrays(gl.TRIANGLES, 12, 24);
+
+			gl.colorMask(true,true,true,true);
+
+			break;
+		case 3:
+			gl.viewport(0, 0, canvas.width/2, canvas.height);
+			StereoProjection(-6, 6, -4.8, 4.8, 12.99, -100, 0, 13, -0.1);
+
+			gl.activeTexture(gl.TEXTURE0);
+			gl.bindTexture(gl.TEXTURE_2D, texture1);
+			gl.drawArrays(gl.TRIANGLES, 0, 12);
+			// gl.activeTexture(gl.TEXTURE1);
+			gl.bindTexture(gl.TEXTURE_2D, texture2);
+			gl.drawArrays(gl.TRIANGLES, 12, 24);
+
+			gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height);
+			StereoProjection(-6, 6, -4.8, 4.8, 12.99, -100, 0, 13, 0.1);
+			gl.activeTexture(gl.TEXTURE0);
+			gl.bindTexture(gl.TEXTURE_2D, texture1);
+			gl.drawArrays(gl.TRIANGLES, 0, 12);
+			// gl.activeTexture(gl.TEXTURE1);
+			gl.bindTexture(gl.TEXTURE_2D, texture2);
+			gl.drawArrays(gl.TRIANGLES, 12, 24);
+			break;
+	}
+
 	ustawKamere();
-
-gl.clearColor(0, 0, 0, 1);
-gl.clear(gl.COLOR_BUFFER_BIT);
-// gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, texture1);
-	gl.drawArrays(gl.TRIANGLES, 0, 12);
-	// gl.activeTexture(gl.TEXTURE1);
-	gl.bindTexture(gl.TEXTURE_2D, texture2);
-	gl.drawArrays(gl.TRIANGLES, 12, 24);
 
 
 
@@ -292,18 +346,18 @@ window.addEventListener('keydown', function(event) {
 		  window.close();
 	  }
     break;
-
-    // case 38: // Up
-    //   alert('Góra');
-    // break;
-	//
-    // case 39: // Right
-    //   alert('Prawo');
-    // break;
-	//
-    // case 40: // Down
-    //   alert('Dół');
-    // break;
+    case 84: // T
+      tryb = 1;
+		console.log('tryb 1');
+    break;
+    case 89: // Y
+      tryb = 2;
+		console.log('tryb 2');
+    break;
+    case 85: // u
+		console.log('tryb 3');
+      tryb = 3;
+    break;
   }
 }, false);
 
@@ -428,6 +482,52 @@ window.addEventListener('keydown', function(event) {
 		front.z = Math.sin(glm.radians(yaw)) * Math.cos(glm.radians(pitch));
 		cameraFront = glm.normalize(front);
 	}
+
+	function StereoProjection(_left, _right, _bottom, _top, _near, _far, _zero_plane, _dist, _eye)
+	{
+		//    Perform the perspective projection for one eye's subfield.
+//    The projection is in the direction of the negative z-axis.
+		//            _left=-6.0;
+		//            _right=6.0;
+		//            _bottom=-4.8;
+// _top=4.8;
+//    [default: -6.0, 6.0, -4.8, 4.8]
+//    left, right, bottom, top = the coordinate range, in the plane of zero parallax setting,
+//         which will be displayed on the screen.
+//         The ratio between (right-left) and (top-bottom) should equal the aspect
+//    ratio of the display.
+		//                  _near=6.0;
+		//                  _far=-20.0;
+//    [default: 6.0, -6.0]
+//    near, far = the z-coordinate values of the clipping planes.
+		//                  _zero_plane=0.0;
+//    [default: 0.0]
+//    zero_plane = the z-coordinate of the plane of zero parallax setting.
+//    [default: 14.5]
+		//                     _dist=10.5;
+//   dist = the distance from the center of projection to the plane of zero parallax.
+//    [default: -0.3]
+		//                 _eye=-0.3;
+//    eye = half the eye separation; positive for the right eye subfield,
+//    negative for the left eye subfield.
+		let   _dx = _right - _left;
+		let   _dy = _top - _bottom;
+		let   _xmid = (_right + _left) / 2.0;
+		let   _ymid = (_top + _bottom) / 2.0;
+		let   _clip_near = _dist + _zero_plane - _near;
+		let   _clip_far = _dist + _zero_plane - _far;
+		let  _n_over_d = (_clip_near / _dist);
+		let   _topw = _n_over_d * _dy / 2.0;
+		let   _bottomw = -_topw;
+		let   _rightw = _n_over_d * (_dx / 2.0 - _eye);
+		let   _leftw = _n_over_d * (-_dx / 2.0 - _eye);
+		const proj = mat4.create();
+		mat4.frustum(proj, _leftw, _rightw, _bottomw, _topw, _clip_near, _clip_far)
+		mat4.translate(proj, proj, [-_xmid - _eye, -_ymid, 0]);
+		let uniProj = gl.getUniformLocation(program, 'proj');
+		gl.uniformMatrix4fv( uniProj, false, proj);
+	}
+
 
 
 
